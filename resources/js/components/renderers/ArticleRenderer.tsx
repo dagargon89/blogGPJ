da560e1c024bea66a8dc08e2582b413f4698373c
dagargon89 <dagargon89@gmail.com>
2026-04-09 17:05:38 -0500

    feat: add persistent connection option for MySQL and create production environment configuration

--- resources/js/components/renderers/ArticleRenderer.tsx
+++ resources/js/components/renderers/ArticleRenderer.tsx
@@ -1,3 +1,6 @@
+/** Clase compartida con el editor de preview del admin */
+export const ARTICLE_PROSE_CLASSNAME = 'article-body';
+
 interface ArticleRendererProps {
     content: string;
 }
@@ -5,7 +8,7 @@ interface ArticleRendererProps {
 export function ArticleRenderer({ content }: ArticleRendererProps) {
     return (
         <div
-            className="article-body"
+            className={ARTICLE_PROSE_CLASSNAME}
             dangerouslySetInnerHTML={{ __html: content }}
         />
     );
